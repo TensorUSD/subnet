60a3695477fe065b504c08b664e61f74f11ebf23
tensorusd09 <tensorusd09@gmail.com>
2026-08-19 00:34:31 +0545

    leftover push

--- tensorusd/utils/config.py
+++ tensorusd/utils/config.py
@@ -370,7 +370,7 @@ def add_validator_args(cls, parser, mech_id: int = 0):
         type=str,
         help="Backend URL for TensorUSD Agent. Required for running agent validator.",
         default=os.getenv("TENSORUSD_SN_BACKEND_URL", None),
-        required=is_required_arg("ORACLE_CONTRACT_ADDRESS", False, 1, mech_id),
+        required=is_required_arg("TENSORUSD_SN_BACKEND_URL", False, 1, mech_id),
     )
     parser.add_argument(
         "--agent.http_timeout",
